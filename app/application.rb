dae773aa0820625e869885dd4370c3ab3374603a
Min Kim <mk4555@nyu.edu>
2018-01-11 23:52:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    time = Time.new
+    time = Time.now
     resp.write "Current time is: #{time.inspect}\n"
     if time.hour < 12
       resp.write "Good Morning!"
